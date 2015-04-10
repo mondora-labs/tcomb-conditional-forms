e890fa990b31daf49088a59946a1574d5dc151b5
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-04-10 22:54:38 +0200

    Refactored gulpfile (used tcomb-input-collection's).

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,56 @@
 var browserSync = require("browser-sync");
 var gulp        = require("gulp");
+var ghPages     = require("gulp-gh-pages");
 var rename      = require("gulp-rename");
 var mocha       = require("gulp-spawn-mocha");
-var watch       = require("gulp-watch");
 var webpack     = require("gulp-webpack");
 
 /*
-*   Task to run unit tests
+*   Build tasks
 */
-gulp.task("tests", function () {
-    var runTests = function () {
-        gulp.src(["./tests/unit/**/*.jsx", "./tests/unit/**/*.js"])
-            .pipe(mocha({
-                compilers: ".:./tests/compiler.js",
-                reporter: "mochawesome",
-                noExit: true,
-                env: {
-                    NODE_PATH: "./src/",
-                    MOCHAWESOME_REPORTDIR: "./builds/tests/",
-                    MOCHAWESOME_REPORTNAME: "index"
-                }
-            }))
-            .on("error", function (ignore) {
-                // Failing tests count as errors, ignore them
-            })
-            .pipe(reload({stream: true}));
-    };
-    watch([
-        "./src/**/*.jsx",
-        "./src/**/*.js",
-        "./tests/unit/**/*.jsx",
-        "./tests/unit/**/*.js"
-    ], runTests);
-    runTests();
+gulp.task("run-tests", function () {
+    return gulp.src("./tests/unit/**/*.js")
+        .pipe(mocha({
+            compilers: ".:./tests/compiler.js",
+            reporter: "mochawesome",
+            noExit: true,
+            env: {
+                NODE_PATH: "./src/",
+                MOCHAWESOME_REPORTDIR: "./builds/tests/",
+                MOCHAWESOME_REPORTNAME: "index"
+            }
+        }))
+        .on("error", function (ignore) {
+            // Failing tests count as errors, ignore them
+        })
+        .pipe(reload({stream: true}));
+});
+gulp.task("build-example-html", function () {
+    return gulp.src("./example/main.html")
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest("./builds/example/"))
+        .pipe(reload({stream: true}));
+});
+gulp.task("build-example-js", function () {
+    return gulp.src("./example/main.jsx")
+        .pipe(webpack({
+            module: {
+                loaders: [{
+                    test: /\.jsx$/,
+                    loader: "jsx-loader"
+                }]
+            },
+        }))
+        .pipe(rename("app.js"))
+        .pipe(gulp.dest("./builds/example/assets/js/"))
+        .pipe(reload({stream: true}));
 });
 
 /*
-*   Task to run the example
-*/
-gulp.task("example", function () {
-    var buildHtml = function () {
-        gulp.src("./example/main.html")
-            .pipe(rename("index.html"))
-            .pipe(gulp.dest("./builds/example/"))
-            .pipe(reload({stream: true}));
-    };
-    var buildJs = function () {
-        return gulp.src("./example/main.jsx")
-            .pipe(webpack({
-                module: {
-                    loaders: [{
-                        test: /\.jsx$/,
-                        loader: "jsx-loader"
-                    }]
-                },
-            }))
-            .pipe(rename("app.js"))
-            .pipe(gulp.dest("./builds/example/assets/js/"))
-            .pipe(reload({stream: true}));
-    };
-    watch("./example/main.html", buildHtml);
-    buildHtml();
-    watch("./example/main.jsx", buildJs);
-    buildJs();
-});
-
-/*
-*   Task to setup the development server
+*   Setup the development environment
 */
 var reload = browserSync.reload;
-gulp.task("serve", function() {
+gulp.task("dev", ["run-tests", "build-example-html", "build-example-js"], function() {
     browserSync({
         server: {
             baseDir: "./builds/",
@@ -80,9 +61,26 @@ gulp.task("serve", function() {
         injectChanges: false,
         notify: false
     });
+    gulp.watch([
+        "./src/**/*.jsx",
+        "./tests/unit/**/*.jsx",
+        "./example/main.html",
+        "./example/main.jsx"
+    ], [
+        "run-tests",
+        "build-example-html",
+        "build-example-js"
+    ]);
+});
+
+/*
+*   Task to deploy to github pages
+*/
+gulp.task("gh-pages", ["run-tests", "build-example-html", "build-example-js"], function() {
+    return gulp.src("./builds/**/*").pipe(ghPages());
 });
 
 /*
 *   Default task
 */
-gulp.task("default", ["tests", "example", "serve"]);
+gulp.task("default", ["dev"]);
